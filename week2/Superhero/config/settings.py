27aaf4547e529833fd3df5ee6f2b46d4863b7159
Mark Seaman <mark.seaman@imac.net>
2021-09-03 08:47:39 -0600

    Superhero app code

--- week2/Superhero/config/settings.py
+++ week2/Superhero/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from os.path import join
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [join(BASE_DIR, 'static'), ]
