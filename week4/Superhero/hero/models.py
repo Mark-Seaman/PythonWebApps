35c5e87aafb315782555929550ddd2fef36f6824
Mark Seaman <mark.seaman@imac.net>
2021-09-11 13:41:15 -0600

    Add Hero data model

--- week4/Superhero/hero/models.py
+++ week4/Superhero/hero/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Hero (models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.CharField(max_length=200)
