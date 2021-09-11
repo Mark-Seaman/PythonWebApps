d82c5355b3fcfcd421564dfbbed9a7b0d342d545
Mark Seaman <mark.seaman@imac.net>
2021-09-11 11:18:32 -0600

    Add simple test

--- week4/Superhero/hero/tests.py
+++ week4/Superhero/hero/tests.py
@@ -1,3 +1,7 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
 
-# Create your tests here.
+
+class SimpleTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
