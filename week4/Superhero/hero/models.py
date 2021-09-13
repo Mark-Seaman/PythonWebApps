dde20bad78f29117f77fee3e5e33133b8c9a4e63
Mark Seaman <mark.seaman@imac.net>
2021-09-13 09:16:09 -0600

    Add string representation to Hero

--- week4/Superhero/hero/models.py
+++ week4/Superhero/hero/models.py
@@ -5,3 +5,6 @@ class Hero (models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     image = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'{self.name}'
