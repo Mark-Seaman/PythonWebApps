3c5c0276bb1818786edabb83df0f9f7101f26311
Mark Seaman <mark.seaman@imac.net>
2021-09-15 09:56:23 -0600

    Collect static assets in App

--- week4/Superhero/config/settings.py
+++ week4/Superhero/config/settings.py
@@ -120,5 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = '/static/'
+STATIC_ROOT = BASE_DIR / "static_assets"
 STATICFILES_DIRS = [BASE_DIR / "static"]
