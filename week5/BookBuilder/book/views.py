325b9dad0ce80e8050aa4cadbc9b708f0a2f3c4f
Mark Seaman <mark.seaman@imac.net>
2021-09-21 17:43:27 -0600

    Implement List and Detail views

--- week5/BookBuilder/book/views.py
+++ week5/BookBuilder/book/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 
-# Create your views here.
+from .models import Book
+
+
+class BookView(ListView):
+    template_name = 'book_list.html'
+    model = Book
+
+
+class BookDetailView(DetailView):
+    template_name = 'book_detail.html'
+    model = Book
--- week5/BookBuilder/config/urls.py
+++ week5/BookBuilder/config/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from book.views import BookView, BookDetailView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', BookView.as_view()),
+    path('<int:pk>', BookDetailView.as_view()),
 ]
