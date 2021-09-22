7d0c6bfbca8e2f003a9904859a81b4194ac82f89
Mark Seaman <mark.seaman@imac.net>
2021-09-21 18:11:09 -0600

    Add tests

--- week5/BookBuilder/book/tests.py
+++ week5/BookBuilder/book/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Book
 
-# Create your tests here.
+
+class BookTest(TestCase):
+
+    def test_django(self):
+        self.assertTrue
+
+    def test_num_book(self):
+        self.assertEqual(len(Book.objects.all()), 0)
+
+    def test_add_book(self):
+        Book.objects.create(title='Tale of 2 Cities', author='Chuck Dickens')
+        self.assertEqual(len(Book.objects.all()), 1)
