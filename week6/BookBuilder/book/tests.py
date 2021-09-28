2dd3b39e3f480c48b005cdb620658b4fa74182ad
Mark Seaman <mark.seaman@imac.net>
2021-09-28 14:46:54 -0600

    Update tests

--- week6/BookBuilder/book/tests.py
+++ week6/BookBuilder/book/tests.py
@@ -12,4 +12,25 @@ class BookTest(TestCase):
 
     def test_add_book(self):
         Book.objects.create(title='Tale of 2 Cities', author='Chuck Dickens')
-        self.assertEqual(len(Book.objects.all()), 1)
+        Book.objects.create(title='Iliad', author='Homer')
+        self.assertEqual(len(Book.objects.all()), 2)
+
+    def test_book_title(self):
+        Book.objects.create(title='Iliad', author='Homer')
+        b = Book.objects.get(pk=1)
+        self.assertEqual(b.title, 'Iliad')
+        self.assertEqual(b.author, 'Homer')
+
+    def test_book_edit(self):
+        Book.objects.create(title='Iliad', author='Homer')
+        b = Book.objects.get(pk=1)
+        b.author = 'Mark Seaman'
+        b.save()
+        self.assertEqual(b.title, 'Iliad')
+        self.assertEqual(b.author, 'Mark Seaman')
+
+    def test_book_edit(self):
+        Book.objects.create(title='Iliad', author='Homer')
+        b = Book.objects.get(pk=1)
+        b.delete()
+        self.assertEqual(len(Book.objects.all()), 0)
