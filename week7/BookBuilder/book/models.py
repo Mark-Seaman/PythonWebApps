33c2b0fea100929e549b119da1b9d8862ef35534
Mark Seaman <mark.seaman@imac.net>
2021-10-01 11:12:14 -0600

    Copy BookBuilder code from week6 to week7

---
+++ week7/BookBuilder/book/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.urls.base import reverse_lazy
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'{self.pk} - {self.title} by {self.author}'
+
+    def get_absolute_url(self):
+        return reverse_lazy('book_detail', args=[str(self.id)])
