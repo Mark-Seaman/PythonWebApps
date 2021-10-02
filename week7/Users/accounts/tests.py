7d0abbfa1c7a00878646684db411966221435c2f
Mark Seaman <mark.seaman@imac.net>
2021-10-02 09:54:33 -0600

    Add user account test

--- week7/Users/accounts/tests.py
+++ week7/Users/accounts/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+def create_test_user():
+    return get_user_model().objects.create_user(
+        username='TEST_DUDE', email='me@here.com', password='secret',
+        first_name='Testy', last_name='Sensei')
+
+
+class TestCase(TestCase):
+
+    def test_accounts(self):
+        user = create_test_user()
+        self.assertEqual(user.email, 'me@here.com')
+        self.assertEqual(len(User.objects.all()), 1)
