d722027ea879eea6ea3e2582cc13477075c93aba
Mark Seaman <mark.seaman@imac.net>
2021-10-05 09:55:08 -0600

    Sign up template

---
+++ week7/Users/ToDo.md
@@ -0,0 +1,20 @@
+# To Do User Accounts
+
+## Test
+* User page theme
+* Login
+* Logout
+* Signup
+
+
+## Fix
+
+## Extend
+* User page theme
+* Login
+* Logout
+* Signup
+
+
+## Improve
+* Save for next session
--- week7/Users/accounts/tests.py
+++ week7/Users/accounts/tests.py
@@ -3,18 +3,18 @@ from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
 
 
-def create_test_user():
-    return get_user_model().objects.create_user(
-        username='TEST_DUDE', email='me@here.com', password='secret',
-        first_name='Testy', last_name='Sensei')
+# def create_test_user():
+#     return get_user_model().objects.create_user(
+#         username='TEST_DUDE', email='me@here.com', password='secret',
+#         first_name='Testy', last_name='Sensei')
 
 
-class TestData(TestCase):
+# class TestData(TestCase):
 
-    def test_accounts(self):
-        user = create_test_user()
-        self.assertEqual(user.email, 'me@here.com')
-        self.assertEqual(len(User.objects.all()), 1)
+#     def test_accounts(self):
+#         user = create_test_user()
+#         self.assertEqual(user.email, 'me@here.com')
+#         self.assertEqual(len(User.objects.all()), 1)
 
 
 class TestViews(TestCase):
