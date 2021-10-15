fe2d313e8ef5418ef2bbb2912ea5f5087fc8c74c
Mark Seaman <mark.seaman@imac.net>
2021-10-14 18:54:58 -0600

    Add chapter tests

--- week9/BookBuilder/book/tests_chapter.py
+++ week9/BookBuilder/book/tests_chapter.py
@@ -2,13 +2,27 @@ from book.models import Book
 from django.test import TestCase
 from django.urls import reverse
 
-from .models import Book
+from .models import Chapter
 
 
 class ChapterDataTest(TestCase):
 
-    def test_django(self):
-        self.assertTrue
+    def create_chapter(self, title):
+        return Chapter.objects.create(book=self.book, title=title, order=1,
+                                      markdown="# Best of Times", document='tale.md')
+
+    def setUp(self):
+        self.book = Book.objects.create(title='Tale of 2 Cities', author='Chuck Dickens')
+        self.create_chapter('Best of Times')
+
+    def test_add_chapter(self):
+        self.assertEqual(len(Chapter.objects.all()), 1)
+        chapter = self.create_chapter('Worst of Times')
+        chapter.order = 2
+        chapter.markdown = "# Worst of Times"
+        chapter.document = 'tale2.md'
+        chapter.save()
+        self.assertEqual(len(Chapter.objects.all()), 2)
 
 
 class ChapterViewsTest(TestCase):
