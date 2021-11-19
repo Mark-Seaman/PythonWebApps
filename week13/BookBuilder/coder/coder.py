0410d48b698bdb0856070752668fadbd050b77fd
Mark Seaman <mark.seaman@imac.net>
2021-11-19 11:27:56 -0700

    Test values

--- week13/BookBuilder/coder/coder.py
+++ week13/BookBuilder/coder/coder.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 
 from book.book import import_all_books
+from book.models import Book
+from pprint import pformat
 
 
 def create_test_user():
@@ -15,9 +17,25 @@ def create_test_user():
 
 def quick_test():
     print("QUICK TEST")
-    initialize_database()
+    # initialize_database()
 
 
 def initialize_database():
     create_test_user()
     import_all_books()
+
+
+def test_values():
+    # Query for books
+    books = Book.objects.all()
+
+    # Convert to values list
+    for book in books.values_list():
+        print(book)
+
+    # Convert to dictionary
+    for book in books.values():
+        print(book)
+
+    # Print with indents
+    print(pformat(book, indent=4))
