e0d884b49f236e62ccc3a2f265b597ebfb67308e
Mark Seaman <mark.seaman@imac.net>
2022-05-28 09:56:47 -0600

    Add week3/ProfileApp/pages

--- week3/ProfileApp/config/settings.py
+++ week3/ProfileApp/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-klzr6hd96v!ywwcky$juik@92$xh=c)f$8n=m_2#me&4-)((^^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pages',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
