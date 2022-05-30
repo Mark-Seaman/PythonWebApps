41f225061b5208d30b67cb29a07d1f82f675e2a4
Mark Seaman <mark.seaman@imac.net>
2022-05-30 15:36:17 -0600

    Create view for PhotoList and PhotoDetail

--- week3/Gallery/config/urls.py
+++ week3/Gallery/config/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from photos.views import PhotosView
+from photos.views import PhotoDetailView, PhotoListView
 
 urlpatterns = [
-    path('', PhotosView.as_view()),
+    path('', PhotoListView.as_view()),
+    path('<int:id>', PhotoDetailView.as_view()),
 ]
