64ce68027485a7ca7c4e43e7246b5549196bbdfc
Mark Seaman <mark.seaman@imac.net>
2022-05-30 13:18:47 -0600

    Add images to gallery

--- week3/Gallery/photos/views.py
+++ week3/Gallery/photos/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class PhotosView(TemplateView):
+    template_name = 'photos.html'
+
+    def get_context_data(self, **kwargs):
+        return dict(photos=[1, 2, 3])
