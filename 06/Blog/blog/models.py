af0f270c43738cca343ad405b41a69086cd49586
Mark Seaman <mark.seaman@imac.net>
2022-06-16 14:08:04 -0600

    Demo 6

--- 06/Blog/blog/models.py
+++ 06/Blog/blog/models.py
@@ -1,34 +1,28 @@
 from django.db import models
-from django.urls.base import reverse_lazy
-
-
-class Article(models.Model):
-    # author = models.ForeignKey(Author, on_delete=models.CASCADE, editable=False)
-    title = models.CharField(max_length=100)
-    notes = models.TextField()
-
-    def __str__(self):
-        return f'{self.title}'
-
-    def get_absolute_url(self):
-        return reverse_lazy('article_detail', args=[str(self.id)])
-
-    # @property
-    # def dependents(self):
-    #     return Dependents.objects.filter(parent=self)
+from django.urls import reverse_lazy
 
 
 class Blog(models.Model):
-    # author = models.ForeignKey(Author, on_delete=models.CASCADE, editable=False)
+
     title = models.CharField(max_length=100)
-    notes = models.TextField()
+    author = models.CharField(max_length=100, default='Abe Lincoln')
+    body = models.TextField()
 
     def __str__(self):
         return f'{self.title}'
 
     def get_absolute_url(self):
-        return reverse_lazy('blog_detail', args=[str(self.id)])
+        return reverse_lazy('blog_list')
 
-    # @property
-    # def dependents(self):
-    #     return Dependents.objects.filter(parent=self)
+
+class Article (models.Model):
+
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+
+    def __str__(self):
+        return f'{self.title}'
+
+    def get_absolute_url(self):
+        return reverse_lazy('article_list')
--- 06/Blog/config/urls.py
+++ 06/Blog/config/urls.py
@@ -1,10 +1,16 @@
+from django.contrib import admin
 from django.urls import include, path
-
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    # Admin
+    path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+
+    # Home
+    path('', RedirectView.as_view(url='blog/')),
 
     # Blog
-    path('', BlogListView.as_view()),
     path('blog/', include('blog.urls_blog')),
     path('article/', include('blog.urls_article')),
 
