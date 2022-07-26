ded215ca0e03d9e1cda33760b215c18d2b44910d
Mark Seaman <mark.seaman@imac.net>
2022-07-26 14:37:38 -0600

    Demo 12 - Website Generator

--- 12/CourseBuilder/course/views.py
+++ 12/CourseBuilder/course/views.py
@@ -9,37 +9,9 @@ from markdown import markdown
 from .models import Lesson
 
 
-def get_lesson(i, path):
-    lesson = Lesson.objects.get_or_create(pk=i+1)[0]
-    lesson.title = get_title(path)
-    lesson.save()
-
-
-def get_title(doc):
-    if not doc.exists():
-        return f'Document not found, {doc}'
-    return doc.read_text().split('\n')[0][10:]
-
-
 def get_document(lesson):
     doc = Path(lesson.document).read_text()
     return markdown(doc)
-
-
-def update_lessons():
-    for i, doc in enumerate(sorted(Path('Documents/').iterdir())):
-        get_lesson(i, doc)
-
-
-# class LessonListView(ListView):
-#     template_name = 'lesson/list.html'
-#     model = Lesson
-#     context_object_name = 'lessons'
-
-#     def get_context_data(self, **kwargs):
-#         update_lessons()
-#         kwargs = super().get_context_data(**kwargs)
-#         return kwargs
 
 
 class LessonDetailView(DetailView):
@@ -48,7 +20,6 @@ class LessonDetailView(DetailView):
     context_object_name = 'lesson'
 
     def get_context_data(self, **kwargs):
-        update_lessons()
         kwargs = super().get_context_data(**kwargs)
         lesson = kwargs.get('lesson')
         kwargs['body'] = get_document(lesson)
