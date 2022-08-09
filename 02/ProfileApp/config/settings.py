3901c4fd8f416bc65ef109d518d407c23ff9b3ca
Mark Seaman <mark.seaman@imac.net>
2022-08-09 12:12:13 -0600

    Demo 1, 2

--- 02/ProfileApp/config/settings.py
+++ 02/ProfileApp/config/settings.py
@@ -37,12 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    # Third-party
-    'crispy_forms',
-
-    # My apps
-    'pages',
 ]
 
 MIDDLEWARE = [
