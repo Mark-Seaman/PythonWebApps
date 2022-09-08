6a881d8b8882dcb97313e53d7b3be9c85a5fc3a5
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2022-09-08 12:55:18 -0600

    route homepage

--- Superhero/config/urls.py
+++ Superhero/config/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from hero.views import HulkView, IronManView, BlackWidowView
+from hero.views import HomeView, HulkView, IronManView, BlackWidowView
 
 urlpatterns =  [
+    path('', HomeView.as_view()),
     path('hulk', HulkView.as_view()),
     path('ironman', IronManView.as_view()),
     path('blackwidow', BlackWidowView.as_view()),
