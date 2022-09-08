fb05a6151ed0235610767eb89400968d0cab54cb
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2022-09-08 12:38:48 -0600

    route blackwidow view

--- Superhero/config/urls.py
+++ Superhero/config/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from hero.views import HulkView, IronManView
+from hero.views import HulkView, IronManView, BlackWidowView
 
 urlpatterns =  [
     path('hulk', HulkView.as_view()),
     path('ironman', IronManView.as_view()),
-    # path('blackwidow', BlackWidow.as_view()),
+    path('blackwidow', BlackWidowView.as_view()),
 ]
