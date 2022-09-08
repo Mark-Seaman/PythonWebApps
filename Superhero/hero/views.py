2ff15d80e3a47f38339c194d6cc28aa0b9e65d98
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2022-09-08 12:37:19 -0600

    add ironman view

--- Superhero/hero/views.py
+++ Superhero/hero/views.py
@@ -9,3 +9,13 @@ class HulkView(TemplateView):
             'id': 'Bruce Banner',
             'image': 'static/images/hulk.jpg'
         }
+
+class IronManView(TemplateView):
+    template_name = 'hero.html'
+
+    def get_context_data(self, **kwargs):
+        return {
+            'title': 'Iron Man',
+            'id': 'Tony Stark',
+            'image': 'static/images/iron_man.jpg'
+        }
