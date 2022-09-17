78cbd9fea542c13931d1548d1adee9ad31e0b7bd
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2022-09-16 20:03:22 -0600

    Update urls.py

--- Superhero/config/urls.py
+++ Superhero/config/urls.py
@@ -1,7 +1,9 @@
+from django.views.generic import RedirectView
 from django.urls import path
 from hero.views import HeroView, HeroListView
 
 urlpatterns =  [
-    path('', HeroListView.as_view()),
-    path('<str:name>', HeroView.as_view()),
+    path('', RedirectView.as_view(url='heroes/')),
+    path('heroes/', HeroListView.as_view()),
+    path('heroes/<int:id>', HeroView.as_view()),
 ]
