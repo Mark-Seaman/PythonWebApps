e7d96c082b2caf8b55608bc487295c8d887f9114
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2022-09-16 19:22:19 -0600

    reconfigure dynamic urls

--- Superhero/config/urls.py
+++ Superhero/config/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from hero.views import HomeView, HulkView, IronManView, BlackWidowView
+from hero.views import HeroView, HeroListView
 
 urlpatterns =  [
-    path('', HomeView.as_view()),
-    path('hulk', HulkView.as_view()),
-    path('ironman', IronManView.as_view()),
-    path('blackwidow', BlackWidowView.as_view()),
+    path('', HeroListView.as_view()),
+    path('<str:name>', HeroView.as_view()),
 ]
