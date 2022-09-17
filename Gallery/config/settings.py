82541de3f34f46dbf64ddefed241c268b9597cf5
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2022-09-16 18:11:29 -0600

    Update settings.py

--- Gallery/config/settings.py
+++ Gallery/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7h6##%ntdf*b=2m&i$+ao9+$*7ol%acd*om579dx1dy4r#o%hu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'photos',
 ]
 
 MIDDLEWARE = [
@@ -115,7 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
