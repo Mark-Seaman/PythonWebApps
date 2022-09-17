bb6998e4089626c0d58c2ad97b04947e31d13b50
Emmanuel Fierro <60017920+fierroe297@users.noreply.github.com>
2022-09-16 22:25:55 -0600

    caption debuggin

--- 04/Gallery/photos/views.py
+++ 04/Gallery/photos/views.py
@@ -4,21 +4,29 @@ from django.views.generic import TemplateView
 
 def photo_list():
     def photo_details(i, f):
-        caption = f'Bruce Wayne, Rich sad boy' if i == 0 else None
-        return dict(id=i, file=f, caption=caption)
-
+        if(i == 0):
+            caption = 'Batman, AKA Bruce Wayne, is a rich sad man' if i == 0 else None 
+            return dict(id=i, file=f, caption=caption)
+        if(i == 1):
+            caption = 'Flash, Barry Allen is the fastest man alive' 
+            return dict(id=i, file=f, caption=caption)
+        if(i == 2):
+            caption = 'The Green Lantern, Hal Jordan has the universes most powerful weapon' 
+            return dict(id=i, file=f, caption=caption)
+        if(i == 3):
+            caption = 'SuperMan, Clark Kent thinks glasses can hid an identity' 
+            return dict(id=i, file=f, caption=caption)
+        if(i == 4):
+            caption = 'Wonder Woman, Princess Diana weilds the lasso of truth' 
+            return dict(id=i, file=f, caption=caption)
+        else:
+            return None 
     photos = Path('static/images').iterdir()
     photos = [photo_details(i, f) for i, f in enumerate(photos)]
     return photos
 
-def photo_list():
-    def photo_details(i, f):
-        caption = f'Bruce Wayne, Rich sad boy' if i == 1 else None
-        return dict(id=i, file=f, caption=caption)
 
-    photos = Path('static/images').iterdir()
-    photos = [photo_details(i, f) for i, f in enumerate(photos)]
-    return photos    
+
 
 
 class PhotoListView(TemplateView):
