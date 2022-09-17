ebd8c4474e8479be44d1f98a1ec66a86033ff986
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2022-09-16 18:55:25 -0600

    Update views.py

--- Gallery/photos/views.py
+++ Gallery/photos/views.py
@@ -1,4 +1,28 @@
+from pathlib import Path
 from django.views.generic import TemplateView
 
 class PhotoView(TemplateView):
     template_name = 'photo.html'
+
+    def get_context_data(self, **kwargs):
+        name = kwargs['name']
+        image = f'\static\images\{name}'
+        return {'photo': image}
+
+class PhotoListView(TemplateView):
+    template_name = 'photos.html'
+    
+    def get_context_data(self, **kwargs):
+        photos = Path('static/images').iterdir()
+        photos = [f for f in photos]
+        return dict(photos=photos)
+
+class PhotoDetailView(TemplateView):
+    template_name = 'photo.html'
+
+    def get_context_data(self, **kwargs):
+        i = kwargs['id']
+        photos = Path('static/images').iterdir()
+        photos = [f for f in photos]
+        p = photos[i]
+        return dict(photo=p)
