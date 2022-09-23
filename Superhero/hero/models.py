56ea6a8907f1f4498cb0e2be108ed18823d86fad
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2022-09-23 10:48:20 -0600

    fix image model

--- Superhero/hero/models.py
+++ Superhero/hero/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Hero(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
-    image = models.ImageField(upload_to = "static/images")
+    image = models.CharField(max_length=200)
 
     def __str__(self):
         return f'{self.name}'
