cda25d836d80100f89abc888ff60227c57dcd004
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2022-09-23 10:30:16 -0600

    add image and string print

--- Superhero/hero/models.py
+++ Superhero/hero/models.py
@@ -3,4 +3,7 @@ from django.db import models
 class Hero(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
-    image = models.CharField(max_length=200)
+    image = models.ImageField(upload_to = "static/images")
+
+    def __str__(self):
+        return f'{self.name}'
