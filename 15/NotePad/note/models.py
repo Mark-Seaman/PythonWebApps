e60a6b041de32c290835ba908f10d0e9c444fc1b
Mark Seaman <mark.seaman@imac.net>
2022-09-23 13:17:53 -0600

    Add NotePad app

---
+++ 15/NotePad/note/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+class Note(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    body = models.TextField()
