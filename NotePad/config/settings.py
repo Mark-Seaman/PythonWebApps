e23b591002e644d1f4d4a28965f792dfb18fc3c2
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2022-09-23 09:16:06 -0600

    configure settings

--- NotePad/config/settings.py
+++ NotePad/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4-o7ipu5daofw^opdicuofu)p#_v1-d!(*%n5y!sivk@g%75dl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'note',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
