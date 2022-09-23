1e7788cd9f7952c5ff32411d0532523b69fcf58a
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2022-09-23 09:22:43 -0600

    create note model

--- NotePad/note/models.py
+++ NotePad/note/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    body = models.TextField()
