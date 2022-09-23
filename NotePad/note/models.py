74c786fa6370d727e5f0605cb69c75802210061d
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2022-09-23 09:45:17 -0600

    add method to display note model

--- NotePad/note/models.py
+++ NotePad/note/models.py
@@ -4,3 +4,6 @@ class Note(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     body = models.TextField()
+
+    def __str__(self):
+        return f'{self.pk}. {self.title} - {self.author}'
