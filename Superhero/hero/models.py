2aefb85d2e57bdf0df64ef947bbd055152d4ec2e
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2022-09-23 09:57:42 -0600

    configure admin and superhero data model

--- Superhero/hero/models.py
+++ Superhero/hero/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Hero(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.CharField(max_length=200)
