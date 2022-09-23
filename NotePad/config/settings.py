86ef8959f7908e7a3db662d879d39d7f1c8bfa7e
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2022-09-23 09:49:08 -0600

    collect static admin interface

--- NotePad/config/settings.py
+++ NotePad/config/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = 'static_assets/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
