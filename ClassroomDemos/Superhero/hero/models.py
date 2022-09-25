6b60231c977d8ec61cb30e61c1a7d3fe4f60afa4
Mark Seaman <mark.seaman@imac.net>
2022-09-25 08:06:08 -0600

    Extend Superhero demo

--- ClassroomDemos/Superhero/hero/models.py
+++ ClassroomDemos/Superhero/hero/models.py
@@ -5,6 +5,10 @@ from django.urls import reverse_lazy
 class Superhero(models.Model):
     name = models.CharField(max_length=200)
     identity = models.CharField(max_length=200)
+    description = models.TextField(default="None")
+    image = models.CharField(max_length=200, default="None")
+    strengths = models.CharField(max_length=200, default="None")
+    weaknesses = models.CharField(max_length=200, default="None")
 
     def __str__(self):
         return self.identity
