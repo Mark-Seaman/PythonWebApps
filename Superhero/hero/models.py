334a60e28f4957d7de2e53a42eb3751286ac0407
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2022-09-30 14:24:46 -0600

    Update models.py

--- Superhero/hero/models.py
+++ Superhero/hero/models.py
@@ -1,9 +1,17 @@
+from email.policy import default
 from django.db import models
+from django.urls import reverse_lazy
 
 class Hero(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.TextField()
-    image = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, default="None")
+    identity = models.CharField(max_length=200, default="None")
+    description = models.TextField(default="None")
+    image = models.CharField(max_length=200, default="None")
+    strengths = models.CharField(max_length=200, default="None")
+    weaknesses = models.CharField(max_length=200, default="None")
 
     def __str__(self):
-        return f'{self.name}'
+        return self.identity
+
+    def get_absolute_url(self):
+        return reverse_lazy('hero_list')
