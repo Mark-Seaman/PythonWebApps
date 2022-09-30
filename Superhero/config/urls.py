5b3d13a6467c81a253ff6cf166d3a214dc380cf1
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2022-09-30 14:24:11 -0600

    Update urls.py

--- Superhero/config/urls.py
+++ Superhero/config/urls.py
@@ -1,9 +1,10 @@
-from django.contrib import admin
 from django.urls import path
-from hero.views import HeroView, HeroListView
+from hero.views import HeroCreateView, HeroDeleteView, HeroDetailView, HeroListView, HeroUpdateView
 
 urlpatterns =  [
-    path('admin/', admin.site.urls),
-    path('', HeroListView.as_view()),
-    path('<int:pk>', HeroView.as_view()),
+    path('', HeroListView.as_view(), name='hero_list'),
+    path('<int:pk>', HeroDetailView.as_view(), name='hero_detail'),
+    path('add', HeroCreateView.as_view(), name='hero_add'),
+    path('<int:pk>/', HeroUpdateView.as_view(), name='hero_edit'),
+    path('<int:pk>/delete', HeroDeleteView.as_view(), name='hero_delete')
 ]
