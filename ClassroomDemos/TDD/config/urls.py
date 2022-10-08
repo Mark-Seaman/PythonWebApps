ab4f0263e844517b7695cc2d697266fcef0ef00b
Mark Seaman <mark.seaman@imac.net>
2022-10-08 15:48:20 -0600

    TTD demo code

--- ClassroomDemos/TDD/config/urls.py
+++ ClassroomDemos/TDD/config/urls.py
@@ -1,21 +1,12 @@
-"""config URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from blog.views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # Article
+    path("article/", ArticleListView.as_view(), name="article_list"),
+    path("article/<int:pk>", ArticleDetailView.as_view(), name="article_detail"),
+    path("article/add", ArticleCreateView.as_view(), name="article_add"),
+    path("article/<int:pk>/", ArticleUpdateView.as_view(), name="article_edit"),
+    path("article/<int:pk>/delete", ArticleDeleteView.as_view(), name="article_delete"),
 ]
