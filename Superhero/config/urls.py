ed6918909bd2bfa41b8afc027693b0ff39ce4277
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2022-10-11 13:31:03 -0600

    add admin and login urls

--- Superhero/config/urls.py
+++ Superhero/config/urls.py
@@ -1,10 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from hero.views import HeroCreateView, HeroDeleteView, HeroDetailView, HeroListView, HeroUpdateView
+from django.contrib import admin
 
 urlpatterns =  [
     path('', HeroListView.as_view(), name='hero_list'),
     path('<int:pk>', HeroDetailView.as_view(), name='hero_detail'),
     path('add', HeroCreateView.as_view(), name='hero_add'),
     path('<int:pk>/', HeroUpdateView.as_view(), name='hero_edit'),
-    path('<int:pk>/delete', HeroDeleteView.as_view(), name='hero_delete')
+    path('<int:pk>/delete', HeroDeleteView.as_view(), name='hero_delete'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('admin/', admin.site.urls),
 ]
