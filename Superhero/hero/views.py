d9f6170c1cff9fe13de5d98c0ef5cc1f68ffd50b
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2022-10-11 13:31:21 -0600

    require logging in for managing database

--- Superhero/hero/views.py
+++ Superhero/hero/views.py
@@ -1,6 +1,7 @@
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Hero
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class HeroListView(ListView):
     template_name = "hero/list.html"
@@ -10,17 +11,17 @@ class HeroDetailView(DetailView):
     template_name = "hero/detail.html"
     model = Hero
 
-class HeroCreateView(CreateView):
+class HeroCreateView(LoginRequiredMixin, CreateView):
     template_name = "hero/add.html"
     model = Hero
     fields = "__all__"
 
-class HeroUpdateView(UpdateView):
+class HeroUpdateView(LoginRequiredMixin, UpdateView):
     template_name = "hero/edit.html"
     model = Hero
     fields = "__all__"
 
-class HeroDeleteView(DeleteView):
+class HeroDeleteView(LoginRequiredMixin, DeleteView):
     template_name = "hero/delete.html"
     model = Hero
     success_url = reverse_lazy('hero_list')
