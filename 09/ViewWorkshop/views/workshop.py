18c19339ce12b5c3f760129dcb5475b5148703cb
Mark Seaman <mark.seaman@imac.net>
2022-10-21 14:15:24 -0600

    Remove .md extension

--- 09/ViewWorkshop/views/workshop.py
+++ 09/ViewWorkshop/views/workshop.py
@@ -20,7 +20,7 @@ def accordion_data():
 
 
 def document_card(document):
-    markdown_text = open(f'Documents/{document}.md').read()
+    markdown_text = open(f'Documents/{document}').read()
     link = dict(href='Index', text='Doc Index')
     return dict(body=markdown(markdown_text), file=document, color='bg-primary text-light p-5', width='', link=link)
 
