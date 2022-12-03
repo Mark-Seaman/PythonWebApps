754c51af67e3a1bc6be267caf20e82452e3d4ad1
Emmanuel Fierro <60017920+fierroe297@users.noreply.github.com>
2022-12-02 18:53:13 -0700

    Github conflits

--- Superhero copy/blog/management/commands/pages.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand
-from blog.page import get_web_page
-
-
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        get_web_page("https://shrinking-world.com")
-        get_web_page("http://google.com")
