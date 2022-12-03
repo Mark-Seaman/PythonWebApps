4b628138dd5fe098eae407cf23d44c7d2f1a2c2a
Emmanuel Fierro <60017920+fierroe297@users.noreply.github.com>
2022-12-02 22:22:25 -0700

    commiting changes

--- proj5/hero/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Superhero(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.TextField()
-    image = models.CharField(max_length=200)
--- proj6/config/urls.py
+++ proj6/config/urls.py
@@ -1,7 +1,7 @@
 """config URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,14 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from note.views import NoteDetailView, NoteListView
-
 urlpatterns = [
-
-    # Admin
     path('admin/', admin.site.urls),
-
-    # Note
-    path('', NoteListView.as_view()),
-    path('<int:pk>', NoteDetailView.as_view()),
 ]
